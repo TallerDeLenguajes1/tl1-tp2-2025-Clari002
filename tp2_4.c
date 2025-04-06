0e1d1e8c9c589677e01aa15c123c162879f724f7
Claribel <claridiaz02@gmail.com>
2025-04-06 01:26:54 -0300

    se agrega funcion listarPCs

--- tp2_4.c
+++ tp2_4.c
@@ -14,11 +14,13 @@ struct compu
 
 //funciones
 void cargarDatos(struct compu pc[], int cantidad);
+void listarPCs(struct compu pcs[], int cantidad);
 
 int main()
 {
     struct compu pc[N];
-
+    cargarDatos(pc, N);
+    listarPCs(pc,N);
 
     return 0;
 }
@@ -32,6 +34,19 @@ void cargarDatos(struct compu pc[], int cantidad)
         pc[i].anio=2015 + rand()%10;
         pc[i].cantidad_nucleos=1+rand()%8;
         pc[i].tipo_cpu=tipos[rand()%6];
+    }
+    
+}
+
+void listarPCs(struct compu pcs[], int cantidad)
+{
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("------------PC %d------------- \n", i+1);
+        printf("Velocidad: %d \n", pcs[i].velocidad);
+        printf("Anio: %d\n", pcs[i].anio);
+        printf("Cantidad de nucleos: %d\n", pcs[i].cantidad_nucleos);
+        printf("Tipo de CPU: %s\n\n", pcs[i].tipo_cpu);
     }
     
 }
